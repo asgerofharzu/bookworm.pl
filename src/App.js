f4bed65eabf5eba62c0e81bdb75d62c8536261b1
unknown <asgerofh@gmail.com>
2023-02-21 03:15:52 +0100

    nav ve main hisse hazirdir(tam yox)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Main from './components/Main';
 
 
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <React.Fragment>
             <Header/>
+            <Main/>
       </React.Fragment>
     );
   }
---
+++ src/components/Main/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import "./main.css"
+import Img from './reading.png'
+class Main extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <Container>
+                    <div className='text-1 text'>What book are
+                    you looking for?</div>
+                    <div className='text-3'>
+                        Don't know what to read next? Take a look at our catalog, we have selected the best books for you!
+                    </div>
+                    <button className='btn-main'> Go to catalog</button>
+                    <img src={Img} alt="img" className="img" />
+                </Container>
+            </>
+        );
+    }
+}
+
+export default Main;
